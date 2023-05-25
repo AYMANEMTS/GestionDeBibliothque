e30cbe74687b9e74b006fb3ef4758cb4afa5d9d6
aymane07 <oujdimraymane@gmail.com>
2023-05-25 23:57:01 +0100

    update model utilisateure,livre

--- app/Models/Utilisateure.php
+++ app/Models/Utilisateure.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 class Utilisateure extends Authenticatable
 {
     protected $table = 'utilisateures';
-    protected $fillable = ['username','email','phone','adress','CIN',
+    protected $fillable = ['username','email','phone','adress','CIN','first_name','last_name','profile_img',
         'role','gender','password'];
     use HasFactory;
 }
