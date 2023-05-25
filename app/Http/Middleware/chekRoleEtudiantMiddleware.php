ae015b6aa79c8986bb7aab073974c66e0a86d356
aymane07 <oujdimraymane@gmail.com>
2023-05-25 23:53:18 +0100

    add chekrole for moder and etudiant

---
+++ app/Http/Middleware/chekRoleEtudiantMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class chekRoleEtudiantMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        if ($user->role == 'etudiant'){
+            return redirect('404');
+        }
+        return $next($request);
+    }
+}
