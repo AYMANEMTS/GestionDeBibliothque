c15cb49f155e9d3171319f6cb44c0e109224c327
aymane07 <oujdimraymane@gmail.com>
2023-05-26 14:58:48 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,27 @@ Route::get('logout/',[AuthenticationController::class,'logout'])->name('logout')
 Route::view('404','error404')->name('404');
 
 
-/* Etudiant profile*/
-//Route::view('/','etudiant.index')->name('hompage');
-Route::get('dashbord/',[EtudiantController::class,'dashbord'])->name('dashbord');
-Route::get('profile/{id}',[EtudiantController::class,'profile'])->name('profile')->middleware('auth');
-Route::get('profile/{id}/edit',[EtudiantController::class,'editprofile'])->name('editprofile')->middleware('auth');
-Route::put('profile/{id}/edit',[EtudiantController::class,'updateprofile'])->name('updateprofile')->middleware('auth');
-Route::get('profile/{id}/changepassword',[EtudiantController::class,'rstpass'])->name('rstpass')->middleware('auth');
-Route::post('profile/{id}/changepassword',[EtudiantController::class,'changepass'])->name('changepass')->middleware('auth');
 
-/* Etudiant livres*/
-Route::get('livres',[EtudiantController::class,'books'])->name('books');
-Route::get('livres/detail/{id}',[EtudiantController::class,'showbook'])->name('detail');
-Route::post('livre/{id}/emprunt',[EtudiantController::class,'empruntlivre'])->name('empruntunlivre');
+/* Etudiant */
+Route::group(['middleware' => ['auth']],function (){
+   Route::controller(EtudiantController::class)->group(function (){
+       /* Etudiant profile*/
+       //Route::view('/','etudiant.index')->name('hompage');
+       Route::get('dashbord/','dashbord')->name('dashbord');
+       Route::get('profile/{id}','profile')->name('profile');
+       Route::get('profile/{id}/edit','editprofile')->name('editprofile');
+       Route::put('profile/{id}/edit','updateprofile')->name('updateprofile');
+       Route::get('profile/{id}/changepassword','rstpass')->name('rstpass');
+       Route::post('profile/{id}/changepassword','changepass')->name('changepass');
+
+       /* Etudiant livres*/
+       Route::get('livres','books')->name('books');
+       Route::get('livres/detail/{id}','showbook')->name('detail');
+       Route::post('livre/{id}/emprunt','empruntlivre')->name('empruntunlivre');
+
+   });
+});
+
 
 /* Super Admin */
 Route::group(['middleware' => ['auth','chek.role.etd','chek.role.mdr']],function (){
