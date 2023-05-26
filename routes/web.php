b346d8f07b58f906324ab41271b3b6aa7ea32b01
aymane07 <oujdimraymane@gmail.com>
2023-05-26 13:58:58 +0100

    update routage

--- routes/web.php
+++ routes/web.php
@@ -1,103 +1,87 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\EtudiantController;
+use App\Http\Controllers\SuperAdminController;
+use App\Models\Livre;
+use App\Models\Utilisateure;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-//Route::get('login/',[\App\Http\Controllers\UserController::class,'Vlogin'])->name('formlogin');
-//Route::post('loogin/',[\App\Http\Controllers\UserController::class,'login'])->name('login');
-//Route::get('register/',[\App\Http\Controllers\UserController::class,'signup'])->name('signup');
-//Route::post('register/',[\App\Http\Controllers\UserController::class,'create'])->name('create');
-//Route::get('dashbord/',[\App\Http\Controllers\UserController::class,'dashbord'])->name('dashbord');
-
-/* Etudiant */
 
 
-
-
-Route::view('/','etudiant.index')->name('hompage');
+/* Auth */
 Route::view('login/','auth.login')->name('viewlogin');
-Route::post('login/',[\App\Http\Controllers\EtudiantController::class,'login'])->name('login');
+Route::post('login/',[AuthenticationController::class,'login'])->name('login');
 Route::view('signup/','auth.signup')->name('viewsignup');
-Route::post('signup/',[\App\Http\Controllers\EtudiantController::class,'signup'])->name('signup');
-Route::get('profile/{id}',[\App\Http\Controllers\EtudiantController::class,'profile'])->name('profile')->middleware('auth');
-Route::get('logout/',[\App\Http\Controllers\EtudiantController::class,'logout'])->name('logout')->middleware('auth');
+Route::post('signup/',[AuthenticationController::class,'signup'])->name('signup');
+Route::get('logout/',[AuthenticationController::class,'logout'])->name('logout')->middleware('auth');
+Route::view('404','error404')->name('404');
 
-Route::group(['middleware' => 'auth'],function (){
-/* super admin */
 
-Route::get('admin/users',[\App\Http\Controllers\SuperAdminController::class,'users'])
-    ->name('super.users');
-Route::view('admin/add/user','superAdmin.adduser')->name('admin.add.user');
-Route::post('admin/add/user.vrf',[\App\Http\Controllers\SuperAdminController::class,'adduser'])
-    ->name('admin.add.user.vrf');
-Route::get('admin/user/change.role/{id}',function ($id){
-    $user = \App\Models\Utilisateure::find($id);
-    return view('superAdmin.change_role',compact('user'));
-})->name('admin.change.user.rol');
-Route::post('admin/user/change.role/{id}/vrf',[\App\Http\Controllers\SuperAdminController::class,'changerole'])
-    ->name('admin.user.changerole');
-Route::get('admin/user/delete/{id}',function ($id){
-    $user = \App\Models\Utilisateure::find($id);
-    return view('superAdmin.delete_user',compact('user'));
-})->name('admin.user.delete');
-Route::delete('admin/user/delete/{id}/vrf',[\App\Http\Controllers\SuperAdminController::class,'delete'])
-    ->name('admin.user.delete.vrf');
+/* Etudiant profile*/
+//Route::view('/','etudiant.index')->name('hompage');
+Route::get('dashbord/',[EtudiantController::class,'dashbord'])->name('dashbord');
+Route::get('profile/{id}',[EtudiantController::class,'profile'])->name('profile')->middleware('auth');
+Route::get('profile/{id}/edit',[EtudiantController::class,'editprofile'])->name('editprofile')->middleware('auth');
+Route::put('profile/{id}/edit',[EtudiantController::class,'updateprofile'])->name('updateprofile')->middleware('auth');
+Route::get('profile/{id}/changepassword',[EtudiantController::class,'rstpass'])->name('rstpass')->middleware('auth');
+Route::post('profile/{id}/changepassword',[EtudiantController::class,'changepass'])->name('changepass')->middleware('auth');
 
+/* Etudiant livres*/
+Route::get('livres',[EtudiantController::class,'books'])->name('books');
+Route::get('livres/detail/{id}',[EtudiantController::class,'showbook'])->name('detail');
+Route::post('livre/{id}/emprunt',[EtudiantController::class,'empruntlivre'])->name('empruntunlivre');
+
+/* Super Admin */
+Route::group(['middleware' => ['auth','chek.role.etd','chek.role.mdr']],function (){
+    Route::controller(SuperAdminController::class)->group(function (){
+        Route::prefix('admin/')->name('admin.')->group(function (){
+            Route::get('users','users')->name('users');
+            Route::view('add/user','superAdmin.adduser')->name('add.user');
+            Route::post('add/user','adduser')->name('add.user.vrf');
+            Route::get('user/change.role/{id}',function ($id){
+                $user = Utilisateure::find($id);
+                return view('superAdmin.change_role',compact('user'));
+            })->name('change.user.rol');
+            Route::post('user/change.role/{id}','changerole')->name('user.changerole');
+            Route::get('user/delete/{id}',function ($id){
+                $user = Utilisateure::find($id);
+                return view('superAdmin.delete_user',compact('user'));
+            })->name('user.delete');
+            Route::delete('user/delete/{id}','delete')->name('user.delete.vrf');
+        });
+    });
 });
 
 
-Route::group(['middleware' => 'auth'],function (){
-    /*  admin  */
-    Route::get('moder/dashbord',[\App\Http\Controllers\AdminController::class,'dashbord'])
-        ->name('moder.dashbord')->middleware('auth');
-    Route::get('moder/profile',[\App\Http\Controllers\AdminController::class,'profileModer'])
-        ->name('moder.profile')->middleware('auth');
+/*  Admin  */
+Route::group(['middleware' => ['auth','chek.role.etd']],function (){
+    Route::controller(AdminController::class)->group(function (){
+       Route::name('moder.')->prefix('moder/')->group(function (){
+           Route::get('dashbord','dashbord')->name('dashbord');
+           Route::get('profile','profileModer')->name('profile');
 
-    /*  admin.etudiant  */
-    Route::get('moder/etudiant',[\App\Http\Controllers\AdminController::class,'listEtudiants'])
-        ->name('moder.etudiant')->middleware('auth');
-    Route::view('moder/etudiant/create','admin.create_student')
-        ->name('moder.formCreateEtudient')->middleware('auth');
-    Route::post('moder/etudiant/create/vrf',[\App\Http\Controllers\AdminController::class,'CreateEtudiant'])
-        ->name('moder.etudiant.create')->middleware('auth');
-    Route::delete('moder/etudiant/dellet/{id}',[\App\Http\Controllers\AdminController::class,'delletEtudiant'])
-        ->name('moder.dellet.student')->middleware('auth');
+           /*  admin.etudiant  */
+           Route::get('etudiant','listEtudiants')->name('etudiant');
+           Route::view('etudiant/create','admin.create_student')->name('formCreateEtudient');
+           Route::post('etudiant/create','CreateEtudiant')->name('etudiant.create');
+           Route::delete('etudiant/dellet/{id}','delletEtudiant')->name('dellet.student');
 
-    /*  admin.livre  */
-    Route::get('moder/livres',[\App\Http\Controllers\AdminController::class,'listLivres'])
-        ->name('moder.livre');
-    Route::view('moder/livre/create','admin.create_livre')
-        ->name('moder.livre.FormCreate');
-    Route::post('moder/livre/create/vrf',[\App\Http\Controllers\AdminController::class,'createLivre'])
-        ->name('moder.livre.create');
-    Route::get('moder/livre/update/{id}',[\App\Http\Controllers\AdminController::class,'editLivre'])
-        ->name('moder.livre.update');
-    Route::post('moder/livre/update/{id}/vrf',[\App\Http\Controllers\AdminController::class,'updateLivre'])
-        ->name('moder.livre.update.vrf');
-    Route::get('moder/livre/delete/{id}',function ($id){
-        $livre = \App\Models\Livre::find($id);return view('admin.delete_livre',compact('livre'));})
-        ->name('moder.Formdelete.livre');
-    Route::delete('moder/livre/delete/{id}',[\App\Http\Controllers\AdminController::class,'deleteLivre'])
-        ->name('moder.livre.delete');
-    Route::get('moder/livre/show/{id}',[\App\Http\Controllers\AdminController::class,'detailLivre'])
-        ->name('moder.livre.show');
+           /*  admin.livre  */
+           Route::get('livres','listLivres')->name('livre');
+           Route::view('livre/create','admin.create_livre')->name('livre.FormCreate');
+           Route::post('livre/create','createLivre')->name('livre.create');
+           Route::get('livre/update/{id}','editLivre')->name('livre.update');
+           Route::post('livre/update/{id}','updateLivre')->name('livre.update.vrf');
+           Route::get('livre/delete/{id}',function ($id){
+               $livre = Livre::find($id);return view('admin.delete_livre',compact('livre'));})
+               ->name('Formdelete.livre');
+           Route::delete('livre/delete/{id}','deleteLivre')->name('livre.delete');
+           Route::get('livre/show/{id}','detailLivre')->name('livre.show');
+       });
+    });
 });
-
-
 
 
 
