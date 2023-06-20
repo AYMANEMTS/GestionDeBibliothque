768aabe5489efc2efc3fa8b44bd3b1070d7f0c34
aymane07 <oujdimraymane@gmail.com>
2023-06-20 01:52:34 +0100

    dislike table

---
+++ database/migrations/2023_06_20_000017_create_deslikes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deslikes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id')->nullable();
+            $table->unsignedBigInteger('cmnt_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('utilisateures')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('cmnt_id')->references('id')->on('coments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deslikes');
+    }
+};
