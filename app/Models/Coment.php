893f5d76d5e6737930a758974be54fc66eab96b6
aymane07 <oujdimraymane@gmail.com>
2023-06-20 01:50:38 +0100

    update

--- app/Models/Coment.php
+++ app/Models/Coment.php
@@ -10,7 +10,7 @@ class Coment extends Model
     use HasFactory;
 
     protected $table = 'coments';
-    protected $fillable = ['user_id','post_id','parent_id','body'];
+    protected $fillable = ['user_id','post_id','parent_id','body','likes','dislikes'];
 
 
     public function user()
@@ -21,6 +21,20 @@ class Coment extends Model
     public function replies()
     {
         return $this->hasMany(Coment::class, 'parent_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class,'cmnt_id');
+    }
+    public function deslikes()
+    {
+        return $this->hasMany(Deslike::class,'cmnt_id');
+    }
+
+    public function isLikedByLoggedInUserC()
+    {
+        return $this->likes->where('user_id', auth()->user()->id)->exists();
     }
 }
 
