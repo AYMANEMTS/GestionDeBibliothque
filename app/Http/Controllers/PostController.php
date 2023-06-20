aa5eeeed96efdb34633a34a21d385ec040560b05
aymane07 <oujdimraymane@gmail.com>
2023-06-20 01:47:51 +0100

    like,deslike post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Coment;
+use App\Models\Deslike;
+use App\Models\Like;
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,17 +14,18 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('status','accepter')->latest()->paginate(8);
+        $posts = Post::where('status', 'accepter')->latest()->paginate(8);
 
-        return view('etudiant.posts.posts',compact('posts'));
+        return view('etudiant.posts.posts', compact('posts'));
     }
 
     public function show(string $id)
     {
         $post = Post::findOrFail($id);
-        $cmt = Coment::where('post_id',$id)->where('parent_id',null)->get();
+        $cmt = Coment::where('post_id', $id)->where('parent_id', null)->get();
         $post->viewsCount();
-        return view('etudiant.posts.post_show',compact('post','cmt'));
+
+        return view('etudiant.posts.post_show', compact('post', 'cmt'));
     }
 
     public function store(Request $request, Post $post)
@@ -53,9 +57,6 @@ class PostController extends Controller
     }
 
 
-
-
-
     public function upload(Request $request)
     {
         if ($request->hasFile('upload')) {
@@ -68,7 +69,7 @@ class PostController extends Controller
 
             $url = asset('media/' . $fileName);
 
-            return response()->json(['fileName' => $fileName, 'uploaded'=> 1, 'url' => $url]);
+            return response()->json(['fileName' => $fileName, 'uploaded' => 1, 'url' => $url]);
         }
 
         return response()->json(['uploaded' => false]);
@@ -77,21 +78,22 @@ class PostController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
-        return view('etudiant.posts.post_update',compact('post'));
+        return view('etudiant.posts.post_update', compact('post'));
     }
-    public function update(Request $request,$id)
+
+    public function update(Request $request, $id)
     {
         $post = Post::find($id);
         $data = $request->validate([
             'title' => 'required|max:100',
             'body' => 'required',
-            'image'=>'image|mimes:jpeg,png,jpg,gif|max:2048'
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
-        if ($request->hasFile('image')){
-            $request->validate(['image'=>'image|mimes:jpeg,png,jpg,gif|max:2048']);
+        if ($request->hasFile('image')) {
+            $request->validate(['image' => 'image|mimes:jpeg,png,jpg,gif|max:2048']);
             $image = $request->file('image');
             $imagePath = time() . '_' . $image->getClientOriginalName();
-            $image->move(public_path('images_posts'),$imagePath);
+            $image->move(public_path('images_posts'), $imagePath);
             $post->image = $image;
         }
         $post->update([
@@ -100,10 +102,74 @@ class PostController extends Controller
         ]);
         return to_route('Post.index')->with(['success' => 'votre post est modified ']);
     }
+
     public function destroy($id)
     {
         $post = Post::findOrFail($id);
         $post->delete();
         return to_route('Post.index')->with(['success' => 'votre post est suprime ']);
     }
+
+    public function like(): JsonResponse
+    {
+        $post = Post::find(request()->id);
+
+        if ($post->isLikedByLoggedInUser()) {
+            $res = Like::where([
+                'user_id' => auth()->user()->id,
+                'post_id' => request()->id
+            ])->delete();
+
+            if ($res) {
+                return response()->json([
+                    'count' => Post::find(request()->id)->likes->count()
+                ]);
+            }
+
+        } else {
+            $like = new Like();
+
+            $like->user_id = auth()->user()->id;
+            $like->post_id = request()->id;
+
+            $like->save();
+
+            return response()->json([
+                'count' => Post::find(request()->id)->likes->count()
+            ]);
+        }
+    }
+    public function deslike(): JsonResponse
+    {
+        $post = Post::find(request()->id);
+        $des = Deslike::where('user_id',Auth::user()->id)
+               ->where('post_id',$post->id)->first();
+
+        if ($des) {
+            $res = Deslike::where([
+                'user_id' => auth()->user()->id,
+                'post_id' => request()->id
+            ])->delete();
+
+            if ($res) {
+                return response()->json([
+                    'count' => Post::find(request()->id)->deslikes->count()
+                ]);
+            }
+
+        } else {
+            $deslike = new Deslike();
+
+            $deslike->user_id = auth()->user()->id;
+            $deslike->post_id = request()->id;
+
+            $deslike->save();
+
+            return response()->json([
+                'count' => Post::find(request()->id)->deslikes->count()
+            ]);
+        }
+    }
+
 }
+
